2d3b65d946d4095a9bb86c78b2dcabc75b31c14c
rakutek <rakutek12@gmail.com>
2020-08-31 19:15:16 +0900

    init

---
+++ components/Layout.tsx
@@ -0,0 +1,30 @@
+import React, {ReactNode} from 'react'
+import Head from 'next/head'
+
+type Props = {
+    children?: ReactNode
+    title?: string
+}
+
+const Layout = ({children, title = 'This is the default title'}: Props) => (
+    <div>
+        <Head>
+            <title>{title}</title>
+            <meta charSet="utf-8"/>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+        </Head>
+        <header>
+
+            <p>header</p>
+
+        </header>
+
+        {children}
+
+        <footer>
+            <p>footer</p>
+        </footer>
+    </div>
+)
+
+export default Layout
